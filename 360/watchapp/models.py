c35fad5796360bbc80a9962e2725ea5b15c0eb81
Jamesmc412 <102428028+Jamesmc412@users.noreply.github.com>
2024-10-23 14:46:03 -0500

    added YouTubeData model to models.py

--- 360/watchapp/models.py
+++ 360/watchapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class YouTubeData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_url = models.URLField()
+    video_title = models.CharField(max_length=255)
+    added_at = models.DateTimeField(auto_now_add=True)
+    duration = models.PositiveIntegerField(default=0)  # Set default to 0
+
+    def __str__(self):
+        return self.video_title
