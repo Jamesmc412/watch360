6eb6be49f8d40c2c58ba9939ef69cfab6e7dcdf1
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2024-10-27 19:43:40 -0500

    re-adding the database testing

--- watch360/360/watchapp/urls.py
+++ watch360/360/watchapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import login_view, register_view, homepage_view, settings_view, logout_view, chat_view
+from . import views
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -8,4 +9,10 @@ urlpatterns = [
     path('settings/', settings_view, name='settings'),
     path('logout/', logout_view, name='logout'),
     path('chat/', chat_view, name='chat'),
+    #testing django-friendship
+    path('users/', views.user_list, name='user_list'),
+    path('send-request/<int:user_id>/', views.send_friend_request, name='send_friend_request'),
+    path('accept-request/<int:request_id>/', views.accept_friend_request, name='accept_friend_request'),
+    path('reject-request/<int:request_id>/', views.reject_friend_request, name='reject_friend_request'),
+    path('unfriend/<int:user_id>/', views.unfriend, name='unfriend'),
 ]
