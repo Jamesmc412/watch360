40fec8829f397baee883814d28b2bb9e28ac980c
Sean <125327404+seanb0@users.noreply.github.com>
2024-10-27 19:04:33 -0500

    Wrong Merge Reverting back to old file
    Wrong Merge

--- 360/watchapp/urls.py
+++ 360/watchapp/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('', views.homepage_view, name='homepage'),  # Ensure only one homepage route
+    path('', views.home_view, name='homepage'),  # Matches 'homepage'
     path('settings/', views.settings_view, name='settings'),
     path('logout/', views.logout_view, name='logout'),
     path('search_video/', views.search_video, name='search_video'),
-    path('delete_video/<int:video_id>/', views.delete_video, name='delete_video'),  # Add this line for deleting videos
+    path('delete/<int:video_id>/', views.delete_video, name='delete_video'),
+    path('check-videos-status/', views.check_videos_status, name='check_videos_status'),
 ]
+
