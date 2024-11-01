64f342b885805fc9360e107a8ca74c09f08a976d
Sean <125327404+seanb0@users.noreply.github.com>
2024-11-01 13:03:37 -0500

    Delete watch360/360/watchapp directory
    Replacing with new version

--- watch360/360/watchapp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WatchappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'watchapp'  # Ensure this matches your app's folder name
-
--- watch360/360/watchapp/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from watchapp.consumers import ChatConsumer  # Correct import
-
-# the empty string routes to ChatConsumer, which manages the chat functionality.
-websocket_urlpatterns = [
-    path("", ChatConsumer.as_asgi()),
-]
